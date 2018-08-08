fd09d6cdd3b6614acfbdc7f234da6e2965e717ac
Syy <shirakawa.yy@gmail.com>
2018-08-08 23:45:50 +0900

    Add UnchangeableInPlayingAttribute

---
+++ UnchangeableInPlayingAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[AttributeUsage( AttributeTargets.Field, Inherited = true, AllowMultiple = true)]
+public sealed class UnchangeableInPlayingAttribute : PropertyAttribute
+{
+    public UnchangeableInPlayingAttribute() { }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer( typeof( UnchangeableInPlayingAttribute ) )]
+public sealed class UnchangeableInPlayingDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginDisabledGroup( Application.isPlaying );
+        EditorGUI.PropertyField( position, property, label);
+        EditorGUI.EndDisabledGroup();
+    }
+}
+#endif
