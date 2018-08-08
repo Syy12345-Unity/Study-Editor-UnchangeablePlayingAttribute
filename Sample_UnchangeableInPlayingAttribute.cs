e687ffbad889d5f4433e7752ac19b137ea1f4d56
Syy <shirakawa.yy@gmail.com>
2018-08-08 23:45:57 +0900

    Add Sample

---
+++ Sample_UnchangeableInPlayingAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample_UnchangeableInPlayingAttribute : MonoBehaviour {
+
+    [UnchangeableInPlaying]
+    public float hoge;
+    [UnchangeableInPlaying]
+    public string fuga;
+}
